7c9bda330014ac88b8e421e96c380a4064869df6
Szymon Mrozowski <mrozowskion@gmail.com>
2024-02-11 21:25:09 +0100

    Add test event producer module

---
+++ src/main/kotlin/com/mrozowski/ims/adapter/incoming/KafkaEventConsumer.kt
@@ -0,0 +1,29 @@
+package com.mrozowski.ims.adapter.incoming
+
+import com.mrozowski.ims.domain.InventoryManagementSystemFacade
+import com.mrozowski.ims.domain.model.InventoryEventCommand
+import com.mrozowski.ims.infrastructure.logger
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+
+
+@Component
+
+class KafkaEventConsumer(private val facade: InventoryManagementSystemFacade) {
+    private val logger = logger()
+
+    @KafkaListener(topics = ["test-topic"], groupId = "ims-consumer-group")
+    fun listen(message: InventoryItemEventMessage) {
+        logger.info("Consumer triggered with message: $message")
+        val command = InventoryEventCommand(
+            message.internalItemId,
+            message.itemName,
+            message.amount,
+            InventoryEventCommand.Operation.valueOf(message.operation.toString())
+        )
+
+        facade.processInventoryEvent(command)
+    }
+
+}
+
